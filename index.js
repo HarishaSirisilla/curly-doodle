66d1a661366e5274bb50163cac6dda2d672606b2
Harisha Sirisilla <106546162+HarishaSirisilla@users.noreply.github.com>
2022-06-05 13:54:04 +0530

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-let result_date = (no_of_days) => {
+let result_date = (days) => {
   let addDays = require("date-fns/addDays");
-  let result = addDays(new Date("2020-08-22"), no_of_days);
-  return result;
+  let result = addDays(new Date("2020-08-22"), days);
+  return `${result.getdate()}-${result.getMonth()+1}-${result.getFullYear()}`;
 };
 
-let no_of_days = 10;
-let result = result_date(no_of_days);
-
-module.exports = result;
+module.exports = result_date;
